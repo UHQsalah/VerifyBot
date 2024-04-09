8041ab1cab81b4ed5ddce0f5613bfba3a731a6f4
salahW3b <113988161+salahW3b@users.noreply.github.com>
2024-04-09 07:34:07 +0200

    new version

--- events/verif/interactionCreate.js
+++ events/verif/interactionCreate.js
@@ -2,12 +2,12 @@ module.exports = async (client, db, config, interaction) => {
         try {
 
             if (!interaction.isStringSelectMenu()) return;
-
+            const verif = db.get(`verifrole_${interaction.guild.id}`);
             if (interaction.customId == "select") {
 
                 if (interaction.values[0] == "verifie") {
                     const roles = interaction.guild.roles.cache;
-                    const role = roles.get(`1177987296755191818`);
+                    const role = roles.get(`${verif}`);
                     const member = interaction.member;
                     await member.roles.add(role);
                         interaction.reply({
